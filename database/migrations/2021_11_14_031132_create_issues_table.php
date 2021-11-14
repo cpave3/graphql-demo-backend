9145029a042e3aae65ef17edb28f6dbcefcabdf5
Cameron Pavey <cpave3@gmail.com>
2021-11-14 14:16:48 +1100

    initial commit

---
+++ database/migrations/2021_11_14_031132_create_issues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('assignee_id');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
